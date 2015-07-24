d655f4888bd4a8019611f0111f4421b41af99006
Mohammed Ashour <m.aly.ashour@gmail.com>
2015-07-24 02:10:39 +0200

    Update break_time_reminder.py

--- break_time_reminder.py
+++ break_time_reminder.py
@@ -5,11 +5,14 @@ import ctypes
 
 breaks 					= 0  # Num of breaks token 
 max_breaks 				= 3  # Num of max breaks you can take 
-BREAK_PERIOD_IN_SECONDS	= 10 # BREAK Period 
-ROUND_PERIOD_IN_SECONDS = 30 # Work Period 
-BREAK_MUSIC_LINK 	    	= "https://soundcloud.com/ayman-memo/bkf1tni5eixt" 
-MessageBox = ctypes.windll.user32.MessageBoxA #make MSG Box object 
+BREAK_PERIOD_IN_HOURS	= input ("Enter the break period in Hours : ") # BREAK Period 
+BREAK_PERIOD_IN_SECONDS = BREAK_PERIOD_IN_HOURS * 3600
+ROUND_PERIOD_IN_HOURS   = input ("Enter the work round period in Hours : ") # Work Period 
+ROUND_PERIOD_IN_SECONDS	= ROUND_PERIOD_IN_HOURS * 3600
+BREAK_MUSIC_LINK 		= "https://soundcloud.com/ayman-memo/bkf1tni5eixt"
+MessageBox = ctypes.windll.user32.MessageBoxA
 
+print "Start Working ^_^ "
 while (breaks < max_breaks):
 	time.sleep(ROUND_PERIOD_IN_SECONDS)
 	relax_reminder = MessageBox(None, 'what about taking a break ?', 'Relax Reminder :D', 1)
