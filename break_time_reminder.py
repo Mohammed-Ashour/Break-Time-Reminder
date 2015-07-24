45bca8dae0b69e724464e0e91993f27acd4408d4
Mohammed Ashour <m.aly.ashour@gmail.com>
2015-07-24 02:15:56 +0200

    customize max num of breaks you can take out of the code

--- break_time_reminder.py
+++ break_time_reminder.py
@@ -3,14 +3,14 @@ import webbrowser
 import ctypes
 
 
-breaks 					= 0  # Num of breaks token 
-max_breaks 				= 3  # Num of max breaks you can take 
-BREAK_PERIOD_IN_HOURS	= input ("Enter the break period in Hours : ") # BREAK Period 
+breaks 			= 0  # Num of breaks token 
+max_breaks 		= input ("Enter the  max num of breaks you can take today :  ")  # Num of max breaks you can take 
+BREAK_PERIOD_IN_HOURS	= input ("Enter the break period in Hours ") # BREAK Period 
 BREAK_PERIOD_IN_SECONDS = BREAK_PERIOD_IN_HOURS * 3600
-ROUND_PERIOD_IN_HOURS   = input ("Enter the work round period in Hours : ") # Work Period 
+ROUND_PERIOD_IN_HOURS   = input ("Enter the work round period in Hours ") # Work Period 
 ROUND_PERIOD_IN_SECONDS	= ROUND_PERIOD_IN_HOURS * 3600
-BREAK_MUSIC_LINK 		= "https://soundcloud.com/ayman-memo/bkf1tni5eixt"
-MessageBox = ctypes.windll.user32.MessageBoxA
+BREAK_MUSIC_LINK 	= "https://soundcloud.com/ayman-memo/bkf1tni5eixt"
+MessageBox 		= ctypes.windll.user32.MessageBoxA
 
 print "Start Working ^_^ "
 while (breaks < max_breaks):
@@ -20,6 +20,6 @@ while (breaks < max_breaks):
 		webbrowser.open(BREAK_MUSIC_LINK, new=1, autoraise=True)
 		time.sleep(BREAK_PERIOD_IN_SECONDS)
 		breaks += 1 
-
+	
 
 	
